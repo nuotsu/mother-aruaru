25b4328d0c2a91ae4e473d26d6a009afba8a6645
Mitchell Christ <xzmaru@gmail.com>
2023-04-29 12:22:53 -0700

    add vercel/analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <slot></slot>
 
 <script>
+	import { dev } from '$app/environment'
+	import { inject } from '@vercel/analytics'
 	import '../styles/app.css'
+
+	inject({ mode: dev ? 'development' : 'production' })
 </script>
